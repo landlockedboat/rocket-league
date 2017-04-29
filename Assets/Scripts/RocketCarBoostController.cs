553b4095acd47d38e587a0be64d634e171b12a61
wextia <valcazar96@gmail.com>
2017-04-29 15:46:06 +0200

    Added primitive boost and jump

---
+++ Assets/Scripts/RocketCarBoostController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class RocketCarBoostController : MonoBehaviour {
+
+    [SerializeField]
+    float boostForce = 500;
+
+    Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (CrossPlatformInputManager.GetButton("Fire3"))
+        {
+            // Let us assume perfectly vertical jumps
+            Vector3 force = Vector3.forward;
+            force = transform.rotation * force;
+            force *= boostForce;
+            _rigidbody.AddForce(force, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/RocketCarJumpController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RocketCarJumpController : MonoBehaviour {
+
+    [SerializeField]
+    float jumpForce = 500;
+
+    Rigidbody _rigidbody;
+
+    void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate () {
+        if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+            // Let us assume perfectly vertical jumps
+            Vector3 force = Vector3.up;
+            force *= jumpForce;
+            _rigidbody.AddForce(force, ForceMode.Impulse);
+        }
+	}
+}
