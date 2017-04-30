c8a25f6ee57bb709ed4e73490b128b6f1661efe0
wextia <valcazar96@gmail.com>
2017-04-30 13:17:48 +0200

    Refactored code and added custom classes

---
+++ Assets/Scripts/RocketCarUserControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(RocketCarController))]
+public class RocketCarUserControl : MonoBehaviour
+{
+    private RocketCarController m_Car; // the car controller we want to use
+
+
+    private void Awake()
+    {
+        // get the car controller
+        m_Car = GetComponent<RocketCarController>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        // pass the input to the car!
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+#if !MOBILE_INPUT
+        float handbrake = CrossPlatformInputManager.GetAxis("Fire1");
+        m_Car.Move(h, v, v, handbrake);
+#else
+        m_Car.Move(h, v, v, 0f);
+#endif
+    }
+}
